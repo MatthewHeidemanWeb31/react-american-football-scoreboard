74b9a01b05b939bfd9ea24edfef4c8df83d903e6
matthew-heideman <heidemanmr@yahoo.com>
2020-05-14 16:53:27 -0500

    Started Timer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useEffect  } from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
 function App() {
+  // useEffect(() => {
+    const [ firstTimer, setFirstTimer ] = useState(0)
+    setTimeout(() => {
+        let c = firstTimer
+        setFirstTimer(c+= 1)
+    }, 1000 )
+  // } )
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [ homeScore, setHomeScore ] = useState(0);
   const [ awayScore, setAwayScore ] = useState(0);
@@ -17,7 +24,7 @@ function App() {
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+  <div className="timer">{firstTimer}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
   <div className="away__score">{awayScore}</div>
